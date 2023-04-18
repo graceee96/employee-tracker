908690a98401a13baf7bda5dc969ff03a7bd1242
Grace Roh <grace@BIGMINE.attlocal.net>
2023-04-17 19:25:29 -0700

    fix add Employee function

--- helpers/addEmployee.js
+++ helpers/addEmployee.js
@@ -9,49 +9,73 @@ const db = mysql.createConnection(
         password: '12345678',
         database: 'company_db',
     },
-    console.log(`Connected to the company_db database.`)
-).promise();
+    // console.log(`Connected to the company_db database.`)
+);
 
 function addEmployee() {
-    inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the employee\'s first name?',
-            name: 'new_firstname',
-        },
-        {
-            type: 'input',
-            message: 'What is the employee\'s last name?',
-            name: 'new_lastname',
-        },
-        {
-            type: 'list',
-            message: 'What is the employee\'s role?',
-            name: 'new_role',
-            choices: []
-        },
-        {
-            type: 'list',
-            message: 'Who is the employee\'s manager?',
-            name: 'new_manager',
-            choices: []
-        }
-    ])
-    .then((input) => {
-        console.log(input);
-        const insertEmployee = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
-        const employeeInfo = [input.new_firstname, input.new_lastname, input.new_role, input.new_manager];
+    db.query('SELECT * FROM role', (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            const roles = result.map(a => ({
+                name: a.title,
+                value: a.id
+            }));
+            // console.log(roles)
+            db.query('SELECT * FROM company_db.employee WHERE manager_id IS NULL', (err, result) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    const managers = result.map(a => ({
+                        name: a.first_name + ' ' + a.last_name,
+                        value: a.id
+                    }));
+                    
+                    // console.log(managers);
 
-        db.query(insertEmployee, employeeInfo, (err, results) => {
-            if(err) {
-                console.log(err);
-            } else {
-                console.log(results);
-                console.log(`Successfully added new employee: ${input.new_firstname} ${input.new_lastname}, ${input.new_role}`)
-            }
-        })
-    })
-}
+                    inquirer
+                        .prompt([
+                            {
+                                type: 'input',
+                                message: 'What is the employee\'s first name?',
+                                name: 'new_firstname',
+                            },
+                            {
+                                type: 'input',
+                                message: 'What is the employee\'s last name?',
+                                name: 'new_lastname',
+                            },
+                            {
+                                type: 'list',
+                                message: 'What is the employee\'s role?',
+                                name: 'new_role',
+                                choices: roles,
+                            },
+                            {
+                                type: 'list',
+                                message: 'Who is the employee\'s manager?',
+                                name: 'new_manager',
+                                choices: managers,
+                            }
+                        ])
+                        .then((input) => {
+                            console.log(input);
+                            const insertEmployee = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+                            const employeeInfo = [input.new_firstname, input.new_lastname, input.new_role, input.new_manager];
+
+                            db.query(insertEmployee, employeeInfo, (err, results) => {
+                                if(err) {
+                                    console.log(err);
+                                } else {
+                                    // console.log(results);
+                                    console.log(`Successfully added new employee: ${input.new_firstname} ${input.new_lastname}`)
+                                }
+                            })
+                        });
+                };
+            });
+        };
+    });
+};
 
 module.exports = addEmployee;
--- helpers/viewAllRoles.js
+++ helpers/viewAllRoles.js
@@ -1,6 +1,7 @@
 //import dependencies
 const consoleTable = require('console.table');
 const mysql = require('mysql2');
+const employeeManager = require('../index');
 
 const db = mysql.createConnection(
     {
@@ -16,6 +17,9 @@ function viewAllRoles() {
     const rolesTable = 'SELECT role.id, role.title AS title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id';
 
     db.query(rolesTable, (err, results) => console.log(consoleTable.getTable(results)));
+
+    // employeeManager();
+    // console.log(employeeManager)
 };
 
 module.exports = viewAllRoles;
