294cef33dd1f19870bcaea8050ca628a64bd8686
Grace Roh <grace@BIGMINE.attlocal.net>
2023-04-17 19:51:42 -0700

    make separate employeeManager file

--- index.js
+++ index.js
@@ -13,81 +13,9 @@ const db = mysql.createConnection(
     console.log(`Connected to the company_db database.`)
 );
 
-//import helper functions
-const viewAllDepartments = require('./utils/viewAllDepartments');
-const viewAllRoles = require('./utils/viewAllRoles');
-const viewAllEmployees = require('./utils/viewAllEmployees')
-const addDepartment = require('./utils/addDepartment');
-const addRole = require('./utils/addRole');
-const addEmployee = require('./utils/addEmployee');
+const employeeManager = require('./utils/employeeManager')
 
 
-//inquirer
-function employeeManager() {
-    inquirer
-        .prompt({
-            type: 'list',
-            message: 'What would you like to do?',
-            name: 'task',
-            choices: [
-                'View all departments',
-                'View all roles',
-                'View all employees',
-                'Add a department',
-                'Add a role',
-                'Add an employee',
-                'Update an employee role',
-                'Quit'
-            ],
-        })
-        .then((input) => {
-            // console.log(input);
-            
-            //switch statements
-            switch (input.task) {
-                case 'View all departments':
-                    viewAllDepartments();
-
-                    //employeeManager();
-
-                    break;
-                case 'View all roles':
-                    viewAllRoles();
-
-                    //employeeManager();
-
-                    break;
-                case 'View all employees':
-                    viewAllEmployees();
-
-                    //employeeManager();
-                    break;
-                case 'Add a department':
-                    addDepartment();
-
-                    //employeeManager();
-                    break;
-                case 'Add a role':
-                    addRole();
-
-                    //employeeManager();
-                    break;
-                case 'Add an employee':
-                    addEmployee();
-                    //employeeManager();
-                    break;
-                case 'Update an employee role':
-                    //employeeManager();
-                    break;
-                case 'Quit':
-                    return;
-            };
-
-            // employeeManager();
-        })
-}
-
-module.exports = employeeManager;
 
 //init function - display title & employeeManager()
 function init() {
---
+++ utils/employeeManager.js
@@ -0,0 +1,68 @@
+const inquirer = require('inquirer');
+
+//import helper functions
+const viewAllDepartments = require('./viewAllDepartments');
+const viewAllRoles = require('./viewAllRoles');
+const viewAllEmployees = require('./viewAllEmployees')
+const addDepartment = require('./addDepartment');
+const addRole = require('./addRole');
+const addEmployee = require('./addEmployee');
+
+function employeeManager() {
+    inquirer
+        .prompt({
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'task',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Quit'
+            ],
+        })
+        .then((input) => {
+            // console.log(input);
+            
+            //switch statements
+            switch (input.task) {
+                case 'View all departments':
+                    viewAllDepartments();
+
+                    break;
+                case 'View all roles':
+                    viewAllRoles();
+
+                    break;
+                case 'View all employees':
+                    viewAllEmployees();
+
+                    break;
+                case 'Add a department':
+                    addDepartment();
+
+                    break;
+                case 'Add a role':
+                    addRole();
+
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+
+                    break;
+                case 'Update an employee role':
+                    
+                    break;
+                case 'Quit':
+                    return;
+            };
+
+            // employeeManager();
+        })
+}
+
+module.exports = employeeManager;
